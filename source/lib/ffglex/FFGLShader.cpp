659e9cc9d7d01a571d1d86c988b12195bea58525
mvink <menno@resolume.com>
2018-09-24 17:09:28 +0200

    -Cleaned up FFGLShader class.

--- source/lib/ffglex/FFGLShader.cpp
+++ source/lib/ffglex/FFGLShader.cpp
@@ -1,166 +1,216 @@
 #include "FFGLShader.h"
 #include <stdio.h>
+#include <vector>
+#include <assert.h>
 
 #define LOGSHADERERRORS
 
 namespace ffglex
 {
+/**
+ * The default constructor just initializes this object to represent no shader.
+ */
 FFGLShader::FFGLShader() :
-	m_linkStatus( 0 ),
-	m_glProgram( 0 ),
-	m_glVertexShader( 0 ),
-	m_glFragmentShader( 0 )
+	vertexShaderID( 0 ),
+	fragmentShaderID( 0 ),
+	programID( 0 )
 {
 }
+/**
+ * Destructing this object just checks and lets you know if you've properly released the GL resources.
+ */
 FFGLShader::~FFGLShader()
 {
+	//If any of these assertions hit you forgot to free this shader's gl resources.
+	assert( vertexShaderID == 0 );
+	assert( fragmentShaderID == 0 );
+	assert( programID == 0 );
 }
 
-void FFGLShader::CreateGLResources()
+/**
+ * Compiles and links two shaders into a shader program.
+ *
+ * @param vertexShader: The glsl shader string that represents the shader that should be used in the vertex processing stage.
+ * You should make sure that this glsl string defines the glsl version it's targetting (eg #version 330) and respect the OpenGL specs
+ * for that shader version.
+ * @param fragmentShader: The glsl shader string that represents the shader that should be used in the fragment processing stage.
+ * You should make sure that this glsl string defines the glsl version it's targetting (eg #version 330) and respect the OpenGL specs
+ * for that shader version.
+ * @return: Whether or not compiling/linking succeeded and thus if this object can be used to bind a shader program for rendering.
+ * If compilation succeeded you need to make sure that you're freeing the GL resources when you no longer intend to use this shader.
+ */
+bool FFGLShader::Compile( const std::string& vertexShader, const std::string& fragmentShader )
 {
-	if( m_glProgram == 0 )
-		m_glProgram = glCreateProgram();
-
-	if( m_glVertexShader == 0 )
-		m_glVertexShader = glCreateShader( GL_VERTEX_SHADER_ARB );
-
-	if( m_glFragmentShader == 0 )
-		m_glFragmentShader = glCreateShader( GL_FRAGMENT_SHADER_ARB );
+	return Compile( vertexShader.c_str(), fragmentShader.c_str() );
 }
+/**
+ * Compiles and links two shaders into a shader program.
+ *
+ * @param vertexShader: The glsl shader string that represents the shader that should be used in the vertex processing stage.
+ * You should make sure that this glsl string defines the glsl version it's targetting (eg #version 330) and respect the OpenGL specs
+ * for that shader version.
+ * @param fragmentShader: The glsl shader string that represents the shader that should be used in the fragment processing stage.
+ * You should make sure that this glsl string defines the glsl version it's targetting (eg #version 330) and respect the OpenGL specs
+ * for that shader version.
+ * @return: Whether or not compiling/linking succeeded and thus if this object can be used to bind a shader program for rendering.
+ * If compilation succeeded you need to make sure that you're freeing the GL resources when you no longer intend to use this shader.
+ */
+bool FFGLShader::Compile( const char* vertexShader, const char* fragmentShader )
+{
+	//We need to have valid inputs to be able to compile a shader, validate them here so that below we can just use them without checking.
+	if( vertexShader == nullptr || strlen( vertexShader ) == 0 )
+		return false;
+	if( fragmentShader == nullptr || strlen( fragmentShader ) == 0 )
+		return false;
 
+	//Everything needs to succeed for this to be a usable shader. If anything fails we'll be cleaning up everything
+	//so that we wont keep anything that wont be used alive.
+	if( !CompileVertexShader( vertexShader ) ||
+	    !CompileFragmentShader( fragmentShader ) ||
+	    !LinkProgram() )
+	{
+		FreeGLResources();
+		return false;
+	}
+
+	return true;
+}
+/**
+ * Frees any GL resources that this shader is holding.
+ *
+ * Call this when you no longer intend to use this shader.
+ */
 void FFGLShader::FreeGLResources()
 {
-	if( m_glFragmentShader )
+	if( vertexShaderID != 0 )
 	{
-		glDeleteShader( m_glFragmentShader );
-		m_glFragmentShader = 0;
+		glDeleteShader( vertexShaderID );
+		vertexShaderID = 0;
 	}
 
-	if( m_glVertexShader )
+	if( fragmentShaderID != 0 )
 	{
-		glDeleteShader( m_glVertexShader );
-		m_glVertexShader = 0;
+		glDeleteShader( fragmentShaderID );
+		fragmentShaderID = 0;
 	}
 
-	if( m_glProgram )
+	if( programID != 0 )
 	{
-		glDeleteProgram( m_glProgram );
-		m_glProgram = 0;
+		glDeleteProgram( programID );
+		programID = 0;
 	}
 }
 
+/**
+ * Whether or not previous complilation succeeded and this shader is ready to be used for rendering.
+ */
+bool FFGLShader::IsReady() const
+{
+	return vertexShaderID != 0 && fragmentShaderID != 0 && programID != 0;
+}
+/**
+ * Gets the OpenGL ID that represents the linked shader program.
+ *
+ * If the shader is usable this returns anything other than 0.
+ */
 GLuint FFGLShader::GetGLID() const
 {
-	return m_glProgram;
+	return programID;
+}
+/**
+ * Finds the uniform location that needs to be used to addess a uniform with a certain name.
+ *
+ * @param name: The name of the uniform for which you want to find the location. This name has to be
+ * a uniform that's placed inside any of the shader strings provided to the previous successfull call to Compile.
+ * @return: The uniform's location to be passed into a glUniform{1|2|3|4}{f|i} function call. -1 if no uniform with the
+ * provided name is available.
+ */
+GLint FFGLShader::FindUniform( const char* name ) const
+{
+	return glGetUniformLocation( programID, name );
 }
 
-int FFGLShader::IsReady()
+bool FFGLShader::CompileVertexShader( const char* vertexShader )
 {
-	return ( m_glProgram != 0 && m_glVertexShader != 0 && m_glFragmentShader != 0 && m_linkStatus == 1 );
-}
+	vertexShaderID = glCreateShader( GL_VERTEX_SHADER );
 
-int FFGLShader::Compile( const std::string& vtxProgram, const std::string& fragProgram )
-{
-	return Compile( vtxProgram.c_str(), fragProgram.c_str() );
-}
+	// Load Shader Sources
+	glShaderSource( vertexShaderID, 1, &vertexShader, NULL );
 
-int FFGLShader::Compile( const char* vtxProgram, const char* fragProgram )
-{
-	if( m_glProgram == 0 )
-		CreateGLResources();
+	// Compile The Shaders
+	glCompileShader( vertexShaderID );
 
-	int doLink = 0;
+	GLint compileStatus;
+	glGetShaderiv( vertexShaderID, GL_COMPILE_STATUS, &compileStatus );
 
-	//if we can compile a fragment shader, do it.
-	if( m_glFragmentShader != 0 && m_glProgram != 0 && fragProgram != NULL && fragProgram[ 0 ] != 0 )
+	if( compileStatus != GL_TRUE )
 	{
-		const char* strings[] = { fragProgram, NULL };
-
-		// Load Shader Sources
-		glShaderSource( m_glFragmentShader, 1, strings, NULL );
-
-		// Compile The Shaders
-		glCompileShader( m_glFragmentShader );
-
-		GLint compileSuccess;
-		glGetShaderiv( m_glFragmentShader, GL_COMPILE_STATUS, &compileSuccess );
-
-		if( compileSuccess == GL_TRUE )
-		{
-			//attach it to the program
-			glAttachShader( m_glProgram, m_glFragmentShader );
-			doLink = 1;
-		}
-		else
-		{
-			//get the log so we can peek at the error string
-			char log[ 1024 ];
-			GLsizei returnedLength;
-
-			glGetProgramInfoLog( m_glFragmentShader, sizeof( log ) - 1, &returnedLength, log );
-			log[ returnedLength ] = 0;
-
 #ifdef LOGSHADERERRORS
-			printf( "Fragment Shader error: %s \n", log );
+		GLint logLength;
+		glGetShaderiv( vertexShaderID, GL_INFO_LOG_LENGTH, &logLength );
+		std::vector< GLchar > log( logLength + 1 );
+		memset( log.data(), 0, logLength + 1 );
+		glGetShaderInfoLog( vertexShaderID, logLength, NULL, log.data() );
+
+		printf( "Vertex Shader error: %s \n", log.data() );
 #endif
-			int a;
-			a = 0;
-		}
 	}
 
-	//if we can compile a vertex shader, do it
-	if( m_glVertexShader != 0 && m_glProgram != 0 && vtxProgram != 0 && vtxProgram[ 0 ] != 0 )
-	{
-		const char* strings[] = { vtxProgram, NULL };
-
-		// Load Shader Sources
-		glShaderSource( m_glVertexShader, 1, strings, NULL );
-
-		// Compile The Shaders
-		glCompileShader( m_glVertexShader );
-
-		GLint compileSuccess;
-
-		glGetShaderiv( m_glVertexShader, GL_COMPILE_STATUS, &compileSuccess );
-
-		if( compileSuccess == GL_TRUE )
-		{
-			//attach it to the program
-			glAttachShader( m_glProgram, m_glVertexShader );
-			doLink = 1;
-		}
-		else
-		{
-			//get the log so we can peek at the error string
-			char log[ 1024 ];
-			GLsizei returnedLength;
-			glGetProgramInfoLog( m_glVertexShader, sizeof( log ) - 1, &returnedLength, log );
-			log[ returnedLength ] = 0;
-			int a;
-			a = 0;
-
-#ifdef LOGSHADERERRORS
-			printf( "Vertex Shader error: %s \n", log );
-#endif
-		}
-	}
-
-	//check if linking worked
-	GLint linkSuccess = 0;
-	if( doLink )
-	{
-		// Link The Program Object
-		glLinkProgram( m_glProgram );
-		glGetProgramiv( m_glProgram, GL_LINK_STATUS, &linkSuccess );
-	}
-
-	m_linkStatus = linkSuccess;
-	return linkSuccess;
+	return compileStatus == GL_TRUE;
 }
-
-GLuint FFGLShader::FindUniform( const char* name )
+bool FFGLShader::CompileFragmentShader( const char* vertexShader )
 {
-	return glGetUniformLocation( m_glProgram, name );
+	fragmentShaderID = glCreateShader( GL_FRAGMENT_SHADER );
+
+	// Load Shader Sources
+	glShaderSource( fragmentShaderID, 1, &vertexShader, NULL );
+
+	// Compile The Shaders
+	glCompileShader( fragmentShaderID );
+
+	GLint compileStatus;
+	glGetShaderiv( fragmentShaderID, GL_COMPILE_STATUS, &compileStatus );
+
+	if( compileStatus != GL_TRUE )
+	{
+#ifdef LOGSHADERERRORS
+		GLint logLength;
+		glGetShaderiv( fragmentShaderID, GL_INFO_LOG_LENGTH, &logLength );
+		std::vector< GLchar > log( logLength + 1 );
+		memset( log.data(), 0, logLength + 1 );
+		glGetShaderInfoLog( fragmentShaderID, logLength, NULL, log.data() );
+
+		printf( "Fragment Shader error: %s \n", log.data() );
+#endif
+	}
+
+	return compileStatus == GL_TRUE;
+}
+bool FFGLShader::LinkProgram()
+{
+	programID = glCreateProgram();
+
+	glAttachShader( programID, vertexShaderID );
+	glAttachShader( programID, fragmentShaderID );
+
+	GLint linkStatus = 0;
+	glLinkProgram( programID );
+	glGetProgramiv( programID, GL_LINK_STATUS, &linkStatus );
+
+	if( linkStatus != GL_TRUE )
+	{
+#ifdef LOGSHADERERRORS
+		//get the log so we can peek at the error string
+		char log[ 1024 ];
+		GLsizei returnedLength;
+		glGetProgramInfoLog( fragmentShaderID, sizeof( log ) - 1, &returnedLength, log );
+		log[ returnedLength ] = 0;
+
+		printf( "Fragment Shader error: %s \n", log );
+#endif
+	}
+
+	return linkStatus == GL_TRUE;
 }
 
 }//End namespace ffglex
--- source/lib/ffglex/FFGLShader.h
+++ source/lib/ffglex/FFGLShader.h
@@ -4,29 +4,38 @@
 
 namespace ffglex
 {
-class FFGLShader
+/**
+ * The FFGLShader is a helper class you can use to compile glsl shader strings
+ * and link them together into a shader program. You can then get the program id
+ * and use it to bind the resulting shader to use it for the next rendering operation.
+ */
+class FFGLShader final
 {
 public:
-	FFGLShader();
-	virtual ~FFGLShader();
+	FFGLShader();                                                                        //The default constructor just initializes this object to represent no shader.
+	FFGLShader( const FFGLShader& ) = delete;
+	FFGLShader( FFGLShader&& ) = delete;
+	~FFGLShader();                                                                       //Destructing this object just checks and lets you know if you've properly released the GL resources.
 
-	int IsReady();
+	bool Compile( const std::string& vertexShader, const std::string& fragmentShader );  //Compiles and links two shaders into a shader program.
+	bool Compile( const char* vertexShader, const char* fragmentShader );                //Compiles and links two shaders into a shader program.
+	void FreeGLResources();                                                              //Frees any GL resources that this shader is holding.
 
-	int Compile( const char* vtxProgram, const char* fragProgram );
-	int Compile( const std::string& vtxProgram, const std::string& fragProgram );
+	bool IsReady() const;                                                                //Whether or not previous complilation succeeded and this shader is ready to be used for rendering.
+	GLuint GetGLID() const;                                                              //Gets the OpenGL ID that represents the linked shader program.
+	GLint FindUniform( const char* name ) const;                                         //Finds the uniform location that needs to be used to addess a uniform with a certain name.
 
-	GLuint FindUniform( const char* name );
-	void FreeGLResources();
-
-	GLuint GetGLID() const;
+	FFGLShader& operator=( const FFGLShader& ) = delete;
+	FFGLShader& operator=( FFGLShader&& ) = delete;
 
 private:
-	void CreateGLResources();
+	bool CompileVertexShader( const char* vertexShader );    //Internal utility function that tries to compile a vertex shader and updates the internal vertexShaderID. Doesn't clean up on failure.
+	bool CompileFragmentShader( const char* vertexShader );  //Internal utility function that tries to compile a fragmen shader and updates the internal fragmentShaderID. Doesn't clean up on failure.
+	bool LinkProgram();                                      //Internal utility function that tries to link the two previously compiled vertex and fragment shaders into a program. Doesn't clean up on failure.
 
-	GLuint m_glProgram;
-	GLenum m_glVertexShader;
-	GLenum m_glFragmentShader;
-	GLuint m_linkStatus;
+	GLuint vertexShaderID;    //!< The ID OpenGL gave our vertex shader. 0 for invalid.
+	GLuint fragmentShaderID;  //!< The ID OpenGL gave our fragment shader. 0 for invalid.
+	GLuint programID;         //!< The ID OpenGL gave our shader program. Bind this to use this shader. 0 for invalid.
 };
 
 }//End namespace ffglex
