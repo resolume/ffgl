c9dc16668d17bf17e048a668736a4f3925b3fdf3
flyingrub <flyingrub@protonmail.com>
2019-05-23 15:56:45 +0200

    Update screenshake

--- source/plugins/Screenshake/Screenshake.cpp
+++ source/plugins/Screenshake/Screenshake.cpp
@@ -6,11 +6,13 @@ static PluginInstance p = Effect::createPlugin< Screenshake >( {
 } );
 
 static const std::string fshader = R"(
+uniform float relativeTime;
+uniform vec2 amount;
 void main()
 {
-   	vec3 p3 = vec3(0,0, time*speed)*8.0+8.0;
+   	vec3 p3 = vec3(0,0, relativeTime)*8.0+8.0;
     vec2 noise = vec2(simplex3d(p3),simplex3d(p3+10.));
-	if (random) noise = vec2(random3(p3).x,random3(p3+10.).x)*2.-1.;
+	if (random) noise = vec2(random3(p3).x,random3(p3+1000.).x)*2.-1.;
 	fragColor = vec4( texture( inputTexture, i_uv+noise*amount*0.1  ).rgb, 1.0);
 }
 )";
@@ -19,11 +21,21 @@ Screenshake::Screenshake()
 {
 	include( shader::snippet_id::simplex );
 	setFragmentShader( fshader );
-	addParam( ParamRange::create( "amount", 0.1f, { 0.0, 1.0 } ) );
+	addParam( amountX = ParamRange::create( "amountX", 0.1f, { 0.0, 1.0 } ) );
+	addParam( amountY = ParamRange::create( "amountY", 0.1f, { 0.0, 1.0 } ) );
 	addParam( ParamRange::create( "speed", 0.3f, { 0.0, 3. } ) );
 	addParam( ParamBool::create( "random" ) );
 }
 
 Screenshake::~Screenshake()
 {
+}
+
+void Screenshake::update()
+{
+	auto speedParam = std::dynamic_pointer_cast< ParamRange >( getParam( "speed" ) );
+	float speed     = speedParam->getRealValue();
+	relativeTime += deltaTime * speed;
+	shader.Set( "relativeTime", relativeTime );
+	shader.Set( "amount", amountX->getRealValue(), amountY->getRealValue());
 }
--- source/plugins/Screenshake/Screenshake.h
+++ source/plugins/Screenshake/Screenshake.h
@@ -5,4 +5,9 @@ class Screenshake : public Effect
 public:
 	Screenshake();
 	~Screenshake();
+	void update() override;
+
+private:
+	float relativeTime = 0;
+	ParamRange::Ptr amountX, amountY;
 };
