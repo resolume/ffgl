0332151e37dd73ed6769ab150f109790e44f3926
Menno Vink <mennovink20@hotmail.com>
2019-07-31 10:30:32 +0200

    Moved Gradients effect back to being a bare metal example so that we've got at least one example showing the minimum amount of work you need to do if you dont want to use the param/shader utilities.

--- source/plugins/Gradients/FFGLGradients.cpp
+++ source/plugins/Gradients/FFGLGradients.cpp
@@ -2,12 +2,17 @@
 #include <math.h>//floor
 using namespace ffglex;
 
+#define FFPARAM_Hue1 ( 0 )
+#define FFPARAM_Hue2 ( 1 )
+#define FFPARAM_Saturation ( 2 )
+#define FFPARAM_Brightness ( 3 )
+
 static CFFGLPluginInfo PluginInfo(
 	PluginFactory< FFGLGradients >,// Create method
 	"RS01",                        // Plugin unique ID
 	"Gradient Example",            // Plugin name
 	2,                             // API major version number
-	1,                             // API minor version number
+	0,                             // API minor version number
 	1,                             // Plugin major version number
 	000,                           // Plugin minor version number
 	FF_SOURCE,                     // Plugin type
@@ -15,7 +20,7 @@ static CFFGLPluginInfo PluginInfo(
 	"Resolume FFGL Example"        // About
 );
 
-static const char _vertexShaderCode[] = R"(#version 410 core
+static const char vertexShaderCode[] = R"(#version 410 core
 layout( location = 0 ) in vec4 vPosition;
 layout( location = 1 ) in vec2 vUV;
 
@@ -28,7 +33,7 @@ void main()
 }
 )";
 
-static const char _fragmentShaderCode[] = R"(#version 410 core
+static const char fragmentShaderCode[] = R"(#version 410 core
 uniform vec3 RGBLeft;
 uniform vec3 RGBRight;
 
@@ -42,22 +47,27 @@ void main()
 }
 )";
 
-FFGLGradients::FFGLGradients() 
+FFGLGradients::FFGLGradients() :
+	rgbLeftLocation( -1 ),
+	rgbRightLocation( -1 ),
+	m_Hue1( 0.0f ),
+	m_Hue2( 0.5f ),
+	m_Saturation( 1.0f ),
+	m_Brightness( 1.0f )
 {
 	// Input properties
 	SetMinInputs( 0 );
 	SetMaxInputs( 0 );
 
 	// Parameters
-	addParam( hue1 = Param::create( "Hue 1", 0.5f ) );
-	addParam( hue2 = Param::create( "Hue 2" , 0.0f) );
-	addParam( saturation = Param::create( "Saturation", 1.0f ) );
-	addParam( brightness = Param::create( "Brightness", 1.0f ) );
+	SetParamInfof( FFPARAM_Hue1, "Hue 1", FF_TYPE_STANDARD );
+	SetParamInfof( FFPARAM_Hue2, "Hue 2", FF_TYPE_STANDARD );
+	SetParamInfof( FFPARAM_Saturation, "Saturation", FF_TYPE_STANDARD );
+	SetParamInfof( FFPARAM_Brightness, "Brightness", FF_TYPE_STANDARD );
 }
-
 FFResult FFGLGradients::InitGL( const FFGLViewportStruct* vp )
 {
-	if( !shader.Compile( _vertexShaderCode, _fragmentShaderCode ) )
+	if( !shader.Compile( vertexShaderCode, fragmentShaderCode ) )
 	{
 		DeInitGL();
 		return FF_FAIL;
@@ -68,8 +78,11 @@ FFResult FFGLGradients::InitGL( const FFGLViewportStruct* vp )
 		return FF_FAIL;
 	}
 
-	//FFGL requires us to leave the context in a default state on return
-	resetOpenGLState();
+	//FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
+	ScopedShaderBinding shaderBinding( shader.GetGLID() );
+	rgbLeftLocation  = shader.FindUniform( "RGBLeft" );
+	rgbRightLocation = shader.FindUniform( "RGBRight" );
+
 	//Use base-class init as success result so that it retains the viewport.
 	return CFreeFrameGLPlugin::InitGL( vp );
 }
@@ -77,27 +90,76 @@ FFResult FFGLGradients::ProcessOpenGL( ProcessOpenGLStruct* pGL )
 {
 	float rgb1[ 3 ];
 	//we need to make sure the hue doesn't reach 1.0f, otherwise the result will be pink and not red how it should be
-	float hue1Val = ( hue1->getValue() == 1.0f ) ? 0.0f : hue1->getValue();
-	HSVtoRGB( hue1Val, saturation->getValue(), brightness->getValue(), rgb1[ 0 ], rgb1[ 1 ], rgb1[ 2 ] );
+	float hue1 = ( m_Hue1 == 1.0f ) ? 0.0f : m_Hue1;
+	HSVtoRGB( hue1, m_Saturation, m_Brightness, rgb1[ 0 ], rgb1[ 1 ], rgb1[ 2 ] );
 
 	float rgb2[ 3 ];
-	float hue2Val = ( hue2->getValue() == 1.0f ) ? 0.0f : hue2->getValue();
-	HSVtoRGB( hue2Val, saturation->getValue(), brightness->getValue(), rgb2[ 0 ], rgb2[ 1 ], rgb2[ 2 ] );
+	float hue2 = ( m_Hue2 == 1.0f ) ? 0.0f : m_Hue2;
+	HSVtoRGB( hue2, m_Saturation, m_Brightness, rgb2[ 0 ], rgb2[ 1 ], rgb2[ 2 ] );
 
-	shader.Use();
-	shader.Set( "RGBLeft" , rgb1[ 0 ], rgb1[ 1 ], rgb1[ 2 ] );
-	shader.Set( "RGBRight" , rgb2[ 0 ], rgb2[ 1 ], rgb2[ 2 ] );
+	//FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
+	ScopedShaderBinding shaderBinding( shader.GetGLID() );
+	glUniform3f( rgbLeftLocation, rgb1[ 0 ], rgb1[ 1 ], rgb1[ 2 ] );
+	glUniform3f( rgbRightLocation, rgb2[ 0 ], rgb2[ 1 ], rgb2[ 2 ] );
 
 	quad.Draw();
 
-	//FFGL requires us to leave the context in a default state on return
-	resetOpenGLState();
 	return FF_SUCCESS;
 }
 FFResult FFGLGradients::DeInitGL()
 {
 	shader.FreeGLResources();
 	quad.Release();
+	rgbLeftLocation  = -1;
+	rgbRightLocation = -1;
 
 	return FF_SUCCESS;
+}
+
+FFResult FFGLGradients::SetFloatParameter( unsigned int dwIndex, float value )
+{
+	switch( dwIndex )
+	{
+	case FFPARAM_Hue1:
+		m_Hue1 = value;
+		break;
+	case FFPARAM_Hue2:
+		m_Hue2 = value;
+		break;
+	case FFPARAM_Saturation:
+		m_Saturation = value;
+		break;
+	case FFPARAM_Brightness:
+		m_Brightness = value;
+		break;
+	default:
+		return FF_FAIL;
+	}
+
+	return FF_SUCCESS;
+}
+
+float FFGLGradients::GetFloatParameter( unsigned int index )
+{
+	float retValue = 0.0;
+
+	switch( index )
+	{
+	case FFPARAM_Hue1:
+		retValue = m_Hue1;
+		break;
+	case FFPARAM_Hue2:
+		retValue = m_Hue2;
+		break;
+	case FFPARAM_Saturation:
+		retValue = m_Saturation;
+		break;
+	case FFPARAM_Brightness:
+		retValue = m_Brightness;
+		break;
+	default:
+		break;
+	}
+
+	return retValue;
 }
--- source/plugins/Gradients/FFGLGradients.h
+++ source/plugins/Gradients/FFGLGradients.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <FFGLSDK.h>
 
-class FFGLGradients : public Plugin
+class FFGLGradients : public CFreeFrameGLPlugin
 {
 public:
 	FFGLGradients();
@@ -11,12 +11,18 @@ public:
 	FFResult ProcessOpenGL( ProcessOpenGLStruct* pGL ) override;
 	FFResult DeInitGL() override;
 
+	FFResult SetFloatParameter( unsigned int dwIndex, float value ) override;
+
+	float GetFloatParameter( unsigned int index ) override;
+
 private:
-	Param::Ptr hue1;
-	Param::Ptr hue2;
-	Param::Ptr saturation;
-	Param::Ptr brightness;
+	float m_Hue1;
+	float m_Hue2;
+	float m_Saturation;
+	float m_Brightness;
 
 	ffglex::FFGLShader shader;   //!< Utility to help us compile and link some shaders into a program.
 	ffglex::FFGLScreenQuad quad; //!< Utility to help us render a full screen quad.
+	GLint rgbLeftLocation;
+	GLint rgbRightLocation;
 };
