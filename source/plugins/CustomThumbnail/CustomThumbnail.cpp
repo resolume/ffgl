9cb7c4c57477571a188d5595379f6a6d97bf85ad
ResolumeMenno <menno@resolume.com>
2019-08-30 18:17:54 +0200

    Embedded libpng inside plugin so that it can use a png as it's thumbnail

--- source/plugins/CustomThumbnail/CustomThumbnail.cpp
+++ source/plugins/CustomThumbnail/CustomThumbnail.cpp
@@ -1,6 +1,40 @@
 #include "CustomThumbnail.h"
 #include <math.h>//floor
+#include "Thumb.h"
+#include <png.h>
+//#include <pngconf.h>
+#include <pngstruct.h>
+#include <pnginfo.h>
 using namespace ffglex;
+
+namespace PNGHelpers
+{
+struct Stream
+{
+	const void* start;
+	size_t readPos = 0;
+};
+
+static void __cdecl readCallback( png_structp png, png_bytep data, png_size_t length )
+{
+	Stream& stream = *reinterpret_cast< Stream* >( png_get_io_ptr( png ) );
+	memcpy( data, static_cast< const unsigned char* >( stream.start ) + stream.readPos, length );
+	stream.readPos += length;
+}
+
+struct PNGErrorStruct
+{
+};
+
+static void __cdecl errorCallback( png_structp png_ptr, png_const_charp error_message )
+{
+	throw PNGErrorStruct();
+}
+static void __cdecl warningCallback( png_structp, png_const_charp )
+{
+}
+
+}//End namespace PNGHelpers
 
 enum ParamID
 {
@@ -44,11 +78,144 @@ std::vector< Color > generateThumbnail()
 	 * decode from png to raw RGBA.
 	 */
 	std::vector< Color > thumbnail( THUMBNAIL_WIDTH * THUMBNAIL_HEIGHT );
+	/*
 	for( Color& color : thumbnail )
 	{
 		unsigned char intensity = rand() % 256;
 		color = Color( intensity, intensity, intensity, 255 );
 	}
+	*/
+
+	PNGHelpers::Stream stream;
+	stream.start = THUMBNAIL;
+
+	png_structp pngReadStruct = png_create_read_struct( PNG_LIBPNG_VER_STRING, 0, 0, 0 );
+	if( pngReadStruct == nullptr )
+		return thumbnail;
+
+	try
+	{
+		png_infop pngInfoStruct = png_create_info_struct( pngReadStruct );
+		if( pngInfoStruct == nullptr )
+		{
+			png_destroy_read_struct( &pngReadStruct, 0, 0 );
+			return thumbnail;
+		}
+
+		png_set_error_fn( pngReadStruct, 0, PNGHelpers::errorCallback, PNGHelpers::warningCallback );
+
+		// read the header..
+		png_set_read_fn( pngReadStruct, &stream, PNGHelpers::readCallback );
+
+		png_read_info( pngReadStruct, pngInfoStruct );
+
+		png_uint_32 width = 0, height = 0;
+		int bitDepth = 0, colorType = 0, interlaceType = 0;
+		// read the data from the header
+		png_get_IHDR( pngReadStruct, pngInfoStruct, &width, &height, &bitDepth, &colorType, &interlaceType, 0, 0 );
+
+		if( width != THUMBNAIL_WIDTH )
+			return thumbnail;
+		if( height != THUMBNAIL_HEIGHT )
+			return thumbnail;
+
+		// Expand to a minimum of 8 bits per channel if colour palette.
+		if( colorType == PNG_COLOR_TYPE_PALETTE )
+		{
+			png_set_expand( pngReadStruct );
+
+			// PALETTE mode is always without Alpha channel so we need to add this as well like with normal RGB
+			png_set_add_alpha( pngReadStruct, 0xffffffff, PNG_FILLER_AFTER );
+		}
+
+		if( colorType == PNG_COLOR_TYPE_GRAY_ALPHA )
+		{
+			//expand to RGBA
+			png_set_gray_to_rgb( pngReadStruct );
+		}
+		
+		// Do the same if the colour type is not a palette but the bitdepth is < 8 per channel.
+		if( bitDepth < 8 )
+			png_set_expand( pngReadStruct );
+
+
+		// Not sure why the validity of transparancy data should expand bitdepth, but i'll just leave it here.
+		if( png_get_valid( pngReadStruct, pngInfoStruct, PNG_INFO_tRNS ) )
+			png_set_expand( pngReadStruct );
+
+		// If the format is RGB then add alpha channel and set it to fully opaque even when reading 32bit formats.
+		if( colorType == PNG_COLOR_TYPE_RGB )
+			png_set_add_alpha( pngReadStruct, 0xffffffff, PNG_FILLER_AFTER );
+
+		// update the info in case something changed.
+		png_read_update_info( pngReadStruct, pngInfoStruct );
+
+		// get the new info.
+		png_get_IHDR( pngReadStruct, pngInfoStruct, &width, &height, &bitDepth, &colorType, &interlaceType, 0, 0 );
+
+		bool hasAlphaChan = (colorType & PNG_COLOR_MASK_ALPHA) != 0 || pngInfoStruct->num_trans > 0;
+
+		int pixelDepth = pngInfoStruct->pixel_depth;
+		size_t lineStride = width * (pixelDepth / 8); // pixelDepth is in bits convert to bytes
+
+		//set output vars
+
+		/**
+			* In resapi bitDepth usually means bytesPerPixel. Not always though, but when decoding a png to Pixels object using the PixelsUtils
+			* the Pixels object assumes it to be bytes per pixel. So to make sure that doesn't crash we output something other than bitDepth
+			* through the bitDepth argument.
+			*/
+		//*bitDepth_ = pixelDepth / 8;
+		//*lineStride_ = lineStride;
+		//*width_ = width;
+		//*height_ = height;
+		//*hasAlpha_ = hasAlphaChan;
+
+		/* swap bytes of 16-bit files to most significant byte first */
+		if( bitDepth == 16 )
+			png_set_swap( pngReadStruct );
+
+		/* Load the image into a temp buffer in the pnglib format..
+		*/
+		std::vector< unsigned char > tempBuffer( height * lineStride );
+		std::vector< png_bytep > rows( height );
+
+		for( size_t y = 0; y < height; ++y )
+			rows[ y ] = tempBuffer.data() + lineStride * y;
+
+		try
+		{
+			png_read_image( pngReadStruct, rows.data() );
+			png_read_end( pngReadStruct, pngInfoStruct );
+		}
+		catch( PNGHelpers::PNGErrorStruct& )
+		{
+			png_destroy_read_struct( &pngReadStruct, &pngInfoStruct, 0 );
+			return thumbnail;
+		}
+
+		png_destroy_read_struct( &pngReadStruct, &pngInfoStruct, 0 );
+
+		/* resize the passed in buffer if needed */
+
+		//copy row by row
+		for( int y = 0; y < (int)height; ++y )
+		{
+			const void* rowSrc = rows[ y ];
+			void* rowDst = &thumbnail[ y * width ];
+			memcpy( rowDst, rowSrc, lineStride );
+		}
+		for( Color& color : thumbnail )
+		{
+			unsigned char* channels = reinterpret_cast< unsigned char* >( &color.rgba );
+			std::swap( channels[ 0 ], channels[ 3 ] );
+			std::swap( channels[ 1 ], channels[ 2 ] );
+		}
+	}
+	catch( PNGHelpers::PNGErrorStruct& )
+	{
+	}
+
 	return thumbnail;
 }
 /**
