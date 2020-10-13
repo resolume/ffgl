ff15d24fcfbc6218d178df126e9d0adb960e91ef
Menno Vink <mennovink20@hotmail.com>
2020-10-13 13:18:25 +0200

    Updated gradient example to show how to use with the param types referencing colors.

--- source/plugins/Gradients/FFGLGradients.cpp
+++ source/plugins/Gradients/FFGLGradients.cpp
@@ -2,10 +2,18 @@
 #include <math.h>//floor
 using namespace ffglex;
 
-#define FFPARAM_Hue1 ( 0 )
-#define FFPARAM_Hue2 ( 1 )
-#define FFPARAM_Saturation ( 2 )
-#define FFPARAM_Brightness ( 3 )
+enum ParamType : FFUInt32
+{
+	PT_RED1,
+	PT_GREEN1,
+	PT_BLUE1,
+	PT_ALP1,
+
+	PT_HUE2,
+	PT_SAT2,
+	PT_BRI2,
+	PT_ALP2,
+};
 
 static CFFGLPluginInfo PluginInfo(
 	PluginFactory< FFGLGradients >,// Create method
@@ -34,8 +42,8 @@ void main()
 )";
 
 static const char fragmentShaderCode[] = R"(#version 410 core
-uniform vec3 RGBLeft;
-uniform vec3 RGBRight;
+uniform vec4 RGBALeft;
+uniform vec4 RGBARight;
 
 in vec2 uv;
 
@@ -43,27 +51,29 @@ out vec4 fragColor;
 
 void main()
 {
-	fragColor = vec4( mix( RGBLeft, RGBRight, uv.x ), 1.0 );
+	fragColor = mix( RGBALeft, RGBARight, uv.x );
 }
 )";
 
 FFGLGradients::FFGLGradients() :
 	rgbLeftLocation( -1 ),
-	rgbRightLocation( -1 ),
-	m_Hue1( 0.0f ),
-	m_Hue2( 0.5f ),
-	m_Saturation( 1.0f ),
-	m_Brightness( 1.0f )
+	rgbRightLocation( -1 )
 {
 	// Input properties
 	SetMinInputs( 0 );
 	SetMaxInputs( 0 );
 
+	hsba2.hue = 0.5f;
 	// Parameters
-	SetParamInfof( FFPARAM_Hue1, "Hue 1", FF_TYPE_STANDARD );
-	SetParamInfof( FFPARAM_Hue2, "Hue 2", FF_TYPE_STANDARD );
-	SetParamInfof( FFPARAM_Saturation, "Saturation", FF_TYPE_STANDARD );
-	SetParamInfof( FFPARAM_Brightness, "Brightness", FF_TYPE_STANDARD );
+	SetParamInfof( PT_RED1, "Red 1", FF_TYPE_RED );
+	SetParamInfof( PT_GREEN1, "Green 1", FF_TYPE_GREEN );
+	SetParamInfof( PT_BLUE1, "Blue 1", FF_TYPE_BLUE );
+	SetParamInfof( PT_ALP1, "Alpha 1", FF_TYPE_ALPHA );
+
+	SetParamInfof( PT_HUE2, "Hue 2", FF_TYPE_HUE );
+	SetParamInfof( PT_SAT2, "Saturation 2", FF_TYPE_SATURATION );
+	SetParamInfof( PT_BRI2, "Brightness 2", FF_TYPE_BRIGHTNESS );
+	SetParamInfof( PT_ALP2, "Alpha 2", FF_TYPE_ALPHA );
 }
 FFResult FFGLGradients::InitGL( const FFGLViewportStruct* vp )
 {
@@ -80,27 +90,23 @@ FFResult FFGLGradients::InitGL( const FFGLViewportStruct* vp )
 
 	//FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
 	ScopedShaderBinding shaderBinding( shader.GetGLID() );
-	rgbLeftLocation  = shader.FindUniform( "RGBLeft" );
-	rgbRightLocation = shader.FindUniform( "RGBRight" );
+	rgbLeftLocation  = shader.FindUniform( "RGBALeft" );
+	rgbRightLocation = shader.FindUniform( "RGBARight" );
 
 	//Use base-class init as success result so that it retains the viewport.
 	return CFFGLPlugin::InitGL( vp );
 }
 FFResult FFGLGradients::ProcessOpenGL( ProcessOpenGLStruct* pGL )
 {
-	float rgb1[ 3 ];
-	//we need to make sure the hue doesn't reach 1.0f, otherwise the result will be pink and not red how it should be
-	float hue1 = ( m_Hue1 == 1.0f ) ? 0.0f : m_Hue1;
-	HSVtoRGB( hue1, m_Saturation, m_Brightness, rgb1[ 0 ], rgb1[ 1 ], rgb1[ 2 ] );
-
-	float rgb2[ 3 ];
-	float hue2 = ( m_Hue2 == 1.0f ) ? 0.0f : m_Hue2;
-	HSVtoRGB( hue2, m_Saturation, m_Brightness, rgb2[ 0 ], rgb2[ 1 ], rgb2[ 2 ] );
+	float rgba2[ 4 ];
+	float hue2 = ( hsba2.hue == 1.0f ) ? 0.0f : hsba2.hue;
+	HSVtoRGB( hue2, hsba2.sat, hsba2.bri, rgba2[ 0 ], rgba2[ 1 ], rgba2[ 2 ] );
+	rgba2[ 3 ] = hsba2.alpha;
 
 	//FFGL requires us to leave the context in a default state on return, so use this scoped binding to help us do that.
 	ScopedShaderBinding shaderBinding( shader.GetGLID() );
-	glUniform3f( rgbLeftLocation, rgb1[ 0 ], rgb1[ 1 ], rgb1[ 2 ] );
-	glUniform3f( rgbRightLocation, rgb2[ 0 ], rgb2[ 1 ], rgb2[ 2 ] );
+	glUniform4f( rgbLeftLocation, rgba1.red, rgba1.green, rgba1.blue, rgba1.alpha );
+	glUniform4f( rgbRightLocation, rgba2[ 0 ], rgba2[ 1 ], rgba2[ 2 ], rgba2[ 3 ] );
 
 	quad.Draw();
 
@@ -120,18 +126,32 @@ FFResult FFGLGradients::SetFloatParameter( unsigned int dwIndex, float value )
 {
 	switch( dwIndex )
 	{
-	case FFPARAM_Hue1:
-		m_Hue1 = value;
+	case PT_RED1:
+		rgba1.red = value;
 		break;
-	case FFPARAM_Hue2:
-		m_Hue2 = value;
+	case PT_GREEN1:
+		rgba1.green = value;
 		break;
-	case FFPARAM_Saturation:
-		m_Saturation = value;
+	case PT_BLUE1:
+		rgba1.blue = value;
 		break;
-	case FFPARAM_Brightness:
-		m_Brightness = value;
+	case PT_ALP1:
+		rgba1.alpha = value;
 		break;
+
+	case PT_HUE2:
+		hsba2.hue = value;
+		break;
+	case PT_SAT2:
+		hsba2.sat = value;
+		break;
+	case PT_BRI2:
+		hsba2.bri = value;
+		break;
+	case PT_ALP2:
+		hsba2.alpha = value;
+		break;
+
 	default:
 		return FF_FAIL;
 	}
@@ -141,25 +161,26 @@ FFResult FFGLGradients::SetFloatParameter( unsigned int dwIndex, float value )
 
 float FFGLGradients::GetFloatParameter( unsigned int index )
 {
-	float retValue = 0.0;
-
 	switch( index )
 	{
-	case FFPARAM_Hue1:
-		retValue = m_Hue1;
-		break;
-	case FFPARAM_Hue2:
-		retValue = m_Hue2;
-		break;
-	case FFPARAM_Saturation:
-		retValue = m_Saturation;
-		break;
-	case FFPARAM_Brightness:
-		retValue = m_Brightness;
-		break;
-	default:
-		break;
+	case PT_RED1:
+		return rgba1.red;
+	case PT_GREEN1:
+		return rgba1.green;
+	case PT_BLUE1:
+		return rgba1.blue;
+	case PT_ALP1:
+		return rgba1.alpha;
+
+	case PT_HUE2:
+		return hsba2.hue;
+	case PT_SAT2:
+		return hsba2.sat;
+	case PT_BRI2:
+		return hsba2.bri;
+	case PT_ALP2:
+		return hsba2.alpha;
 	}
 
-	return retValue;
+	return 0.0f;
 }
